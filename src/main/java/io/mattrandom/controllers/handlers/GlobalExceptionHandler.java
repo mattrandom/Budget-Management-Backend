84c5694123fcb2678f970ff877f929e1838d85ad
mattrandom <44298255+mattrandom@users.noreply.github.com>
2022-03-25 15:07:46 +0100

    Handling of exceptions thrown when specifying an incorrect URL endpoint with missing query params

--- src/main/java/io/mattrandom/controllers/handlers/GlobalExceptionHandler.java
+++ src/main/java/io/mattrandom/controllers/handlers/GlobalExceptionHandler.java
@@ -2,7 +2,9 @@ package io.mattrandom.controllers.handlers;
 
 import io.mattrandom.controllers.handlers.dtos.ErrorResponse;
 import io.mattrandom.exceptions.AppUserInvalidCredentialsException;
+import io.mattrandom.exceptions.AssetFilterQueryParamException;
 import io.mattrandom.exceptions.AssetIncorrectException;
+import io.mattrandom.exceptions.ExpenseFilterQueryParamException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,5 +23,17 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ErrorResponse appUserInvalidCredentialsExceptionHandler(AppUserInvalidCredentialsException ex) {
         return new ErrorResponse(HttpStatus.FORBIDDEN.value(), ex.getMessage(), "ac0f60c0-591d-4d44-9604-45d3ccf5610c");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse assetFilterQueryParamExceptionHandler(AssetFilterQueryParamException ex) {
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), ex.getErrorCode());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse expenseFilterQueryParamExceptionHandler(ExpenseFilterQueryParamException ex) {
+        return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), ex.getErrorCode());
     }
 }
